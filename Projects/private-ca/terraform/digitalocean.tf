b76a1d5fb4a7d7a248a79981c22f516f7ccaf757
Adrienne Cohea <adriennecohea@gmail.com>
2018-05-17 07:20:48 -0700

    Scale compute nodes to four (4) and add CA server as its own resource because Terraform interpolation variables are a little weird.

--- Projects/private-ca/terraform/digitalocean.tf
+++ Projects/private-ca/terraform/digitalocean.tf
@@ -26,9 +26,20 @@ variable "counts" {
   type = "map"
 
   default = {
-    "compute" = 1
+    "compute" = 4
     "control" = 3
   }
+}
+
+resource "digitalocean_droplet" "ca" {
+  count              = 1
+  image              = "ubuntu-18-04-x64"
+  size               = "s-1vcpu-1gb"
+  region             = "sfo1"
+  name               = "ca"
+  ssh_keys           = [7172020, 7172181]
+  private_networking = true
+  tags               = ["ca"]
 }
 
 resource "digitalocean_droplet" "control" {
@@ -51,6 +62,11 @@ resource "digitalocean_droplet" "compute" {
   ssh_keys           = [7172020, 7172181]
   private_networking = true
   tags               = ["hashicorp", "compute"]
+}
+
+resource "digitalocean_domain" "ca_fqdn" {
+  name       = "ca.${var.domain}"
+  ip_address = "${digitalocean_droplet.ca.ipv4_address}"
 }
 
 resource "digitalocean_domain" "control_fqns" {
