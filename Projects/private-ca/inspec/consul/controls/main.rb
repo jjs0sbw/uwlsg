4a84c1d33280cea0a740f3430754a42049cc02f2
Adrienne Cohea <adriennecohea@gmail.com>
2018-07-28 18:49:45 -0700

    Add firewall tests.

--- Projects/private-ca/inspec/consul/controls/main.rb
+++ Projects/private-ca/inspec/consul/controls/main.rb
@@ -12,4 +12,15 @@ control 'consul' do
             it { should be_enabled }
             it { should be_running }
     end
+
+    describe iptables(table: 'filter', chain: 'INPUT') do
+        it { should have_rule('-A INPUT -p tcp -m tcp --dport 8300 --tcp-flags FIN,SYN,RST,ACK SYN -m comment --comment "Allow Consul server RPC" -m conntrack --ctstate NEW -j ACCEPT') }
+        it { should have_rule('-A INPUT -p tcp -m tcp --dport 8301 --tcp-flags FIN,SYN,RST,ACK SYN -m comment --comment "Allow Consul Serf LAN" -m conntrack --ctstate NEW -j ACCEPT') }
+        it { should have_rule('-A INPUT -p tcp -m tcp --dport 8302 --tcp-flags FIN,SYN,RST,ACK SYN -m comment --comment "Allow Consul Serf WAN" -m conntrack --ctstate NEW -j ACCEPT') }
+        it { should have_rule('-A INPUT -p tcp -m tcp --dport 8500 --tcp-flags FIN,SYN,RST,ACK SYN -m comment --comment "Allow Consul HTTP" -m conntrack --ctstate NEW -j ACCEPT') }
+        it { should have_rule('-A INPUT -p tcp -m tcp --dport 8600 --tcp-flags FIN,SYN,RST,ACK SYN -m comment --comment "Allow Consul DNS" -m conntrack --ctstate NEW -j ACCEPT') }
+        it { should have_rule('-A INPUT -p udp -m udp --dport 8301 -m comment --comment "Allow Consul Serf LAN" -j ACCEPT') }
+        it { should have_rule('-A INPUT -p udp -m udp --dport 8302 -m comment --comment "Allow Consul Serf WAN" -j ACCEPT') }
+        it { should have_rule('-A INPUT -p udp -m udp --dport 8600 -m comment --comment "Allow Consul DNS" -j ACCEPT') }
+    end
 end
