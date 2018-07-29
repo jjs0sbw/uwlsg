19a6ee42cc7a06c2a9115f69dc58c481d7f08f08
Adrienne Cohea <adriennecohea@gmail.com>
2018-07-29 00:17:45 -0700

    Ensure Vault enforces TLS client auth.

---
+++ Projects/private-ca/inspec/control-plane/controls/vault_enforces_tls_client_auth.rb
@@ -0,0 +1,11 @@
+control 'vault-4' do
+    impact 0.8
+    title 'Ensure Vault requires TLS mutual authentication'
+    desc 'Ensures Vault agents are configured to require client TLS certificates.'
+
+    describe file('/etc/vault/config.hcl') do
+        its('content') { should match(%r{tls_require_and_verify_client_cert = "true"}) }
+    end
+end
+
+
