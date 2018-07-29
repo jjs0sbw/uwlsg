247d101222014f458e60dd3d3fea5c4b789fa05f
Adrienne Cohea <adriennecohea@gmail.com>
2018-07-29 00:37:42 -0700

    Write some more Nomad tests.

--- Projects/private-ca/inspec/control-plane/controls/nomad_has_a_leader.rb
+++ Projects/private-ca/inspec/control-plane/controls/nomad_has_a_leader.rb
@@ -1,9 +1,9 @@
-control 'consul-1' do
+control 'nomad-3' do
     impact 0.8
     title 'Ensure Nomad has elected a leader.'
     desc 'Ensures that the Consul cluster has a leader.'
 
-    describe bash('nomad agent-info | grep leader_addr | awk '{print $3}'') do
+    describe bash('nomad agent-info | grep leader_addr | awk \'{print $3}\'') do
         its('stdout.strip') { should ne '' }
         its('exit_status') { should eq 0 }
     end
