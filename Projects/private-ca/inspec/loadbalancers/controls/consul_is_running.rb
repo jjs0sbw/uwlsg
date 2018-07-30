6eba3d9d8bbe5a42a8e9dd507eb2c10124ec1d39
Adrienne Cohea <adriennecohea@gmail.com>
2018-07-29 19:27:29 -0700

    Add a control that Consul is running in the loadbalancer role.

---
+++ Projects/private-ca/inspec/loadbalancers/controls/consul_is_running.rb
@@ -0,0 +1,28 @@
+control 'consul-1' do
+    impact 0.8
+    title 'Ensure Consul is running correctly'
+    desc 'Ensures the Consul service is running.'
+
+    describe systemd_service('consul') do
+            it { should be_installed }
+            it { should be_enabled }
+            it { should be_running }
+    end
+
+    describe bash('consul members | grep server | grep alive | wc -l') do
+        its('stdout.strip') { should cmp >= 3 }
+        its('exit_status') { should eq 0 }
+    end
+
+    describe port(8301) do
+        it { should be_listening }
+    end
+
+    describe port(8500) do
+        it { should be_listening }
+    end
+
+    describe port(8600) do
+        it { should be_listening }
+    end
+end
