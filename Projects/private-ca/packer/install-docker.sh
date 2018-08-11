0f2aeef0e459eca8585127416347ce86f70ba3ce
Adrienne Cohea <adriennecohea@gmail.com>
2018-08-11 00:09:49 -0700

    Fix this fucking disaster.

--- Projects/private-ca/packer/install-docker.sh
+++ Projects/private-ca/packer/install-docker.sh
@@ -3,7 +3,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic edge\"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic edge"
 apt update -yq
-apt install -y docker-ce
 apt upgrade -yq
+apt install -y docker-ce
